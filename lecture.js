333af785d1906d5a43f2f365a8874d537ce0fa7b
tgvdufuture <101932390+tgvdufuture@users.noreply.github.com>
2023-01-29 19:36:03 +0100

    playlist

--- lecture.js
+++ lecture.js
@@ -2,38 +2,40 @@ const titre = document.querySelector('.titre')
 const suivant = document.querySelector('.suivant')
 const retour = document.querySelector('.retour')
 const play = document.querySelector('.play-pause')
-const audio = document.querySelector('audio')
+const pause = document.querySelector('.pause')
+const audio = document.querySelector('.audio')
 const artiste = document.querySelector('.artiste')
 const key = document.querySelector('.key')
 const img = document.querySelector('.img')
+const lecture = document.querySelector('.lecture')
 
 const liste = [
     {
-        src: "./src/audio-1.mp3",
+        src: "./src/believer.mp3",
         nom: "Believer",
         artiste: "Imagine Dragons",
         img: "./assets/miniatures/Believer.png"
     },
     {
-        src: "./src/audio-2.mp3",
+        src: "./src/better.mp3",
         nom: "Better of alone",
         artiste: "Alice Deejay",
         img: "./assets/miniatures/Better-of-alone.png"
     },    
     {
-        src: "./src/audio-3.mp3",
+        src: "./src/hymn.mp3",
         nom: " Hymn For The Weekend",
         artiste: "Coldplay",
         img: "./assets/miniatures/Hymn-for-the-weekend.png"
     },    
     {
-        src: "./src/audio-5.mp3",
+        src: "./src/eye.mp3",
         nom: "Eye of Tiger ",
         artiste: "Survivor",
         img: "./assets/miniatures/Eye-of-tiger.png"
     },
     {
-        src: "./src/audio-4.mp3",
+        src: "./src/final.mp3",
         nom: "The final countdown ",
         artiste: "Europe",
         img: "./assets/miniatures/The-final-coutdown.png"
@@ -45,14 +47,14 @@ let musiquePlay = false;
 function playMusique(){
     musiquePlay = true;
     audio.play();
+    play.innerHTML = `<img src="./assets/icons/play.png">`
     play.classList.add('active');
-    play.innerHTML = `<img src="assets/icons/play.png">`
 }
 function pauseMusique(){
     musiquePlay = false;
     audio.pause();
-    play.classList.remove('active');
     play.innerHTML = `<img src="./assets/icons/pause.png">`
+    play.classList.remove('active');
 }
 
 play.addEventListener('click', ()=> (musiquePlay ? pauseMusique() :
@@ -91,6 +93,122 @@ function suivantMusique(){
 
 suivant.addEventListener('click', suivantMusique);
 
-// function espace(event){
-//     alert("ca marche")
-// }
+
+audio.addEventListener("ended", suivantMusique)
+
+
+
+// lecture Better
+
+let image = document.querySelector('.play')
+let Better = document.querySelector('.better')
+
+musiquePlayBetter = false;
+
+function playMusiqueBetter(){
+    musiquePlayBetter = true;
+    Better.play();
+    image.src = "./assets/icons/play.png";
+    image.classList.add('active');
+}
+function pauseMusiqueBetter(){
+    musiquePlayBetter = false;
+    Better.pause();
+    image.src = "./assets/icons/pause.png";
+    image.classList.remove('active');
+}
+
+image.addEventListener('click', ()=> (musiquePlayBetter ? pauseMusiqueBetter() :
+playMusiqueBetter()));
+
+// lecture hymn
+
+let image2 = document.querySelector('.play2')
+let Hymn = document.querySelector('.hymn')
+
+musiquePlayHymn = false;
+
+function playMusiqueHymn(){
+    musiquePlayHymn = true;
+    Hymn.play();
+    image2.src = "./assets/icons/play.png";
+    image2.classList.add('active');
+}
+function pauseMusiqueHymn(){
+    musiquePlayHymn = false;
+    Hymn.pause();
+    image2.src = "./assets/icons/pause.png";
+    image2.classList.remove('active');
+}
+
+image2.addEventListener('click', ()=> (musiquePlayHymn ? pauseMusiqueHymn() :
+playMusiqueHymn()));
+
+// lecture eye
+
+let image3 = document.querySelector('.play3')
+let Eye = document.querySelector('.eye')
+
+musiquePlayEye = false;
+
+function playMusiqueEye(){
+    musiquePlayEye = true;
+    Eye.play();
+    image3.src = "./assets/icons/play.png";
+    image3.classList.add('active');
+}
+function pauseMusiqueEye(){
+    musiquePlayEye = false;
+    Eye.pause();
+    image3.src = "./assets/icons/pause.png";
+    image3.classList.remove('active');
+}
+
+image3.addEventListener('click', ()=> (musiquePlayEye ? pauseMusiqueEye() :
+playMusiqueEye()));
+
+// lecture final
+
+let image4 = document.querySelector('.play4')
+let Final = document.querySelector('.final')
+
+musiquePlayFinal = false;
+
+function playMusiqueFinal(){
+    musiquePlayFinal = true;
+    Final.play();
+    image4.src = "./assets/icons/play.png";
+    image4.classList.add('active');
+}
+function pauseMusiqueFinal(){
+    musiquePlayFinal = false;
+    Final.pause();
+    image4.src = "./assets/icons/pause.png";
+    image4.classList.remove('active');
+}
+
+image4.addEventListener('click', ()=> (musiquePlayFinal ? pauseMusiqueFinal() :
+playMusiqueFinal()));
+
+// lecture believer
+
+let image5 = document.querySelector('.play5')
+let Believer = document.querySelector('.believer')
+
+musiquePlayBeliever = false;
+
+function playMusiqueBeliever(){
+    musiquePlayBeliever = true;
+    Believer.play();
+    image5.src = "./assets/icons/play.png";
+    image5.classList.add('active');
+}
+function pauseMusiqueBeliever(){
+    musiquePlayBeliever = false;
+    Believer.pause();
+    image5.src = "./assets/icons/pause.png";
+    image5.classList.remove('active');
+}
+
+image5.addEventListener('click', ()=> (musiquePlayBeliever ? pauseMusiqueBeliever() :
+playMusiqueBeliever()));
